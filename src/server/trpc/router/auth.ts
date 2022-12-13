8e3963b8d298eadeae62f2f99fa7f787c5dd6c85
Jon Naske <jonnaske@hotmail.com>
2022-12-13 09:58:26 -0800

    credential auth implemented

--- src/server/trpc/router/auth.ts
+++ src/server/trpc/router/auth.ts
@@ -1,4 +1,6 @@
 import { router, publicProcedure, protectedProcedure } from "../trpc";
+import { z } from "zod";
+import { hash } from 'argon2'
 
 export const authRouter = router({
   getSession: publicProcedure.query(({ ctx }) => {
@@ -7,4 +9,37 @@ export const authRouter = router({
   getSecretMessage: protectedProcedure.query(() => {
     return "You are logged in and can see this secret message!";
   }),
+  signUp: publicProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(8),
+      })
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { name, email, password } = input
+
+      const exists = await ctx.prisma.user.findUnique({
+        where: {
+          email: input.email,
+        },
+      });
+      if (exists) {
+        throw new Error("User already exists");
+      }
+
+      const hashedPassword = await hash(input.password)
+
+
+      const result = await ctx.prisma.user.create({
+        data: { name, email, password: hashedPassword },
+      });
+      return {
+        status: 201,
+        message: "User created",
+        result: result.email
+      }
+
+    }),
 });
